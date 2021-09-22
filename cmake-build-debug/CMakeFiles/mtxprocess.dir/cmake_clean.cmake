7db130e65182f1163d84b8fe89048d6151c97df1
hzr <1791577934@qq.com>
2021-09-22 19:53:34 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/mtxprocess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtxprocess.dir/main.c.o"
+  "mtxprocess"
+  "mtxprocess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mtxprocess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/mtxprocess.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mtxprocess.
+# This may be replaced when dependencies are built.
